44667c4ca0e912a4d4a30b8c706f7538e888f1a5
Gaurav Kumar <gauravkumar803109@gmail.com>
2026-03-11 14:36:17 +0530

    Implement sign-up and sign-in for citizen

---
+++ app/(auth)/citizen/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUpView from '../_components/sign-up-view'
+
+const page = () => {
+    return (
+        <div>
+            <SignUpView />
+        </div>
+    )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='flex items-center justify-center h-screen'>
-      <Button className='bg-primary text-primary-foreground hover:bg-primary/90 cursor-pointer'>
-        Hello World
-      </Button>
+    <div className='flex items-center justify-center h-screen flex-col gap-4'>
+      <h1>GroundUp Pulse</h1>
+      <p>Aap kaun hain?</p>
+
+      <Link href="/citizen/sign-up">
+        <Button variant="outline" className='cursor-pointer'>
+          👤 Citizen
+        </Button>
+      </Link>
+
+      <Link href="/authority/department">
+        <Button variant="outline" className='cursor-pointer'>
+          🏛️ Authority
+        </Button>
+      </Link>
     </div>
   )
 }
