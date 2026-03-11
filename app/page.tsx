80774add92ce9e58a58ab3a6624601882ca598c9
Gaurav Kumar <gauravkumar803109@gmail.com>
2026-03-11 08:59:09 +0530

    Adding shadcn component and changing the theme and also update the browser tab detail

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
     <div className='flex items-center justify-center h-screen'>
-      <h1>Home Page</h1>
+      <Button className='bg-primary text-primary-foreground hover:bg-primary/90 cursor-pointer'>
+        Hello World
+      </Button>
     </div>
   )
 }
