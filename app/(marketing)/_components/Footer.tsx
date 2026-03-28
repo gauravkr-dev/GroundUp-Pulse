71b5c87fa4c4f81f3f177f82132fff59613ab00a
Gaurav Kumar <gauravkumar803109@gmail.com>
2026-03-28 18:46:01 +0530

    Update portfolio url

--- app/(marketing)/_components/Footer.tsx
+++ app/(marketing)/_components/Footer.tsx
@@ -68,7 +68,7 @@ export function Footer() {
                     <a
                         aria-label="x/twitter"
                         className="inline-flex items-center gap-1 text-foreground/80 hover:text-foreground hover:underline"
-                        href={"https://portfolio-silk-rho-58.vercel.app/"}
+                        href={"https://gauravkr-dev.vercel.app/"}
                         rel="noreferrer"
                         target="_blank"
                     >
